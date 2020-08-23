4e7e21ea0c46caf032986d29491ee3b70ada3108
Michiel <Michiel_woute64@hotmail.com>
2020-08-23 18:34:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasisGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("How are you today?");
+        }
+    }
+}
