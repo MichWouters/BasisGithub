16878e91a7cbc67c1cab51f60ef2624cb24d5a22
Michiel <Michiel_woute64@hotmail.com>
2020-09-04 13:40:26 +0200

    Add To Fahrenheit Conversions

--- TemperatureConverter.cs
+++ TemperatureConverter.cs
@@ -18,14 +18,14 @@ namespace BasisGithub
 
         public double KelvinToFahrenheit(double valueToConvert, int precision = 2)
         {
-            // TODO 
-            return -1;
+            double converted = 1.8 * (valueToConvert - 273.15) + 32;
+            return Math.Round(converted, precision);
         }
 
         public double CelsiusToFahrenheit(double valueToConvert, int precision = 2)
         {
-            // TODO 
-            return -1;
+            double converted = (valueToConvert * 1.8) + 32;
+            return Math.Round(converted, precision);
         }
 
         public double FahrenheitToKelvin(double valueToConvert, int precision = 2)
