c79e0dda3b42753b45c4d68fc73d4306acedaf96
Michiel <Michiel_woute64@hotmail.com>
2020-09-05 12:30:02 +0200

    Add DistanceConverter with Placeholders

---
+++ DistanceConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasisGithub
+{
+    public static class DistanceHelper
+    {
+        public static double MilesToKilometers(double valueToConvert, int precision = 2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double KilometersToMiles(double valueToConvert, int precision = 2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double MetersToKilometers(double valueToConvert, int precision = 2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
