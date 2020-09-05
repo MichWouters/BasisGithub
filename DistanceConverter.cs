729448b5417ed2b2660d58a283cdc2c90244f8c4
Michiel <Michiel_woute64@hotmail.com>
2020-09-05 20:28:02 +0200

    Revert

--- DistanceConverter.cs
+++ DistanceConverter.cs
@@ -27,13 +27,5 @@ namespace BasisGithub
 
             return converted;
         }
-
-        public double KilometersToMeters(double valueToConvert, int precision = 2)
-        {
-            double converted = valueToConvert * 1000;
-            converted = Math.Round(converted, precision);
-
-            return converted;
-        }
     }
 }
