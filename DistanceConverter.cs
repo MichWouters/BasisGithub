8d0370edde6da9d1a6fb6d51797697481edad6c3
Michiel <Michiel_woute64@hotmail.com>
2020-09-05 12:33:08 +0200

    Fill in Methods

--- DistanceConverter.cs
+++ DistanceConverter.cs
@@ -2,21 +2,38 @@
 
 namespace BasisGithub
 {
-    public static class DistanceHelper
+    public class DistanceHelper
     {
-        public static double MilesToKilometers(double valueToConvert, int precision = 2)
+        public double MilesToKilometers(double valueToConvert, int precision = 2)
         {
-            throw new NotImplementedException();
+            double converted = valueToConvert * 1.61;
+            converted = Math.Round(converted, precision);
+
+            return converted;
         }
 
-        public static double KilometersToMiles(double valueToConvert, int precision = 2)
+        public double KilometersToMiles(double valueToConvert, int precision = 2)
         {
-            throw new NotImplementedException();
+            double converted = valueToConvert * 0.62;
+            converted = Math.Round(converted, precision);
+
+            return converted;
         }
 
-        public static double MetersToKilometers(double valueToConvert, int precision = 2)
+        public double MetersToKilometers(double valueToConvert, int precision = 2)
         {
-            throw new NotImplementedException();
+            double converted = valueToConvert / 1000;
+            converted = Math.Round(converted, precision);
+
+            return converted;
+        }
+
+         public double KilometersToMeters(double valueToConvert, int precision = 2)
+        {
+            double converted = valueToConvert * 1000;
+            converted = Math.Round(converted, precision);
+
+            return converted;
         }
     }
 }
