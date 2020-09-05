014fc58112bb7f0bca7c17a5d032c59a53547245
Michiel <Michiel_woute64@hotmail.com>
2020-09-05 13:00:27 +0200

    Implement DistanceConverter with 5 digits precision

--- DistanceConverter.cs
+++ DistanceConverter.cs
@@ -6,17 +6,26 @@ namespace BasisGithub
     {
         public double MilesToKilometers(double valueToConvert, int precision = 2)
         {
-            throw new NotImplementedException();
+            double converted = valueToConvert * 1.60934;
+            converted = Math.Round(converted, precision);
+
+            return converted;
         }
 
         public double KilometersToMiles(double valueToConvert, int precision = 2)
         {
-            throw new NotImplementedException();
+            double converted = valueToConvert * 0.62137;
+            converted = Math.Round(converted, precision);
+
+            return converted;
         }
 
         public double MetersToKilometers(double valueToConvert, int precision = 2)
         {
-            throw new NotImplementedException();
+            double converted = valueToConvert / 1000;
+            converted = Math.Round(converted, precision);
+
+            return converted;
         }
     }
 }
